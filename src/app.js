a1164b8c3f6635987aea264776db73ef0324a6cc
tazmania06709 <Josman Vicens>
2024-10-25 08:24:00 -0400

    fixes: changed mongodb in mongo

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json()); // Parseador de Bodies
 
 // Aca conectamos la base de datos
-mongoose.connect(process.env.MONGODB_URL,
+mongoose.connect(process.env.MONGO_URL,
     { dbName: process.env.MONGO_DB_NAME }) 
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
